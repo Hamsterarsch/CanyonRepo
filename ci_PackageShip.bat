f2877dd3b8404d6bd4b152156d14f28a7722b383
Robert Bellin <rl-bellin@web.de>
2019-04-30 15:54:15 +0200

    -

--- ci_PackageShip.bat
+++ ci_PackageShip.bat
@@ -1,1 +1,1 @@
-C:\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -verbose -platform=Win64 -clientconfig=Shipping -serverconfig=Shipping -noP4 -cook -allmaps -build -stage -prereqs -pak -archieve -archivedirectory=C:\ueproj\packaged -project=C:\ueproj\Canyon\Canyon.uproject
+C:\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -CookInEditor=false -verbose -platform=Win64 -clientconfig=Shipping -serverconfig=Shipping -noP4 -cook -allmaps -build -stage -prereqs -pak -archieve -archivedirectory=C:\ueproj\packaged -project=C:\ueproj\Canyon\Canyon.uproject
