d54d018bf5a3222dd6ba81382b3c453703e4f0ca
Robert Bellin <rl-bellin@web.de>
2019-05-20 08:50:19 +0200

    Added wwise plugin and wwiseext module

---
+++ Canyon/Source/Wwiseext/Public/WwiseextBpLib.cpp
@@ -0,0 +1,1 @@
+#include "WwiseextBpLib.h"
---
+++ Canyon/Source/Wwiseext/Public/WwiseextBpLib.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WwiseextBpLib.generated.h"
+
+UCLASS()
+class UWwiseextBpLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	
+
+};
