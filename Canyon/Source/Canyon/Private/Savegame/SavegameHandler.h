34875a3371fc8b63ec826535fd3c0a106a3ee52a
Robert Bellin <rl-bellin@web.de>
2019-07-17 18:25:16 +0200

    Added savegame filepath generation and data saving

--- Canyon/Source/Canyon/Private/Savegame/SavegameHandler.h
+++ Canyon/Source/Canyon/Private/Savegame/SavegameHandler.h
@@ -9,10 +9,33 @@ class USavegameHandler : public UObject
 	GENERATED_BODY()
 
 public:
-	void SaveGame();
+	void SaveGame(bool bIsAutosave = false, int32 SlotNumber = 0) const;
+
+	void LoadNewestSavegame(bool bIncludeAutosaves = true, int32 SlotNumber = 0) const;
 
 
 private:
+	FString BuildSavegameFilepath(bool bIsAutosave, int32 SlotNumber) const;
 
+	FString GetSavegameBasePath() const;
+
+	void AddSavegameFileExtensionToPath(FString &Path) const;
+
+	bool GetNewestSavegameFilepathForSlot(FString &out_Path, bool bIncludeAutosaves, int32 SlotNumber = 0) const;
+
+	FString GenerateSavegameName(bool bIsAutoSave) const;
+
+
+	static bool SaveToFile(const TArray<uint8> &Data, const FString &Path);
+
+	static void AddSavegameSlotToPath(FString &Path, int32 SavegameSlot);
+
+	
+
+
+	FString m_SavegamePathCustomPart{ TEXT("Studio403/Helios/") };
+	FString m_SavegameExtension{ TEXT("nbbsav") };
+	FString m_AutosaveToken{ TEXT("auto") };
 
 };
+
