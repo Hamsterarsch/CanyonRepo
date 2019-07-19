ba6e440b240249cb768d5e972902865d97b181e2
Robert Bellin <rl-bellin@web.de>
2019-07-19 10:54:35 +0200

    started moving scene root implementation

---
+++ Canyon/Source/Canyon/Private/Components/MovingSceneRoot.cpp
@@ -0,0 +1,81 @@
+#include "Components/MovingSceneRoot.h"
+#include "Components/ArrowComponent.h"
+#include "Curves/CurveFloat.h"
+
+UMovingSceneRoot::UMovingSceneRoot() :
+	m_OnePosition{ 0, 1, 0 },
+	m_OneMinusPosition{ 0, -1, 0 },
+	m_CurrentIntervalPos{ 0 }
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+#if WITH_EDITOR
+
+	m_pToOneArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ToOneArrow"));
+	m_pToOneArrow->SetupAttachment(this);
+	SpanArrowComponent(RelativeLocation, m_OnePosition, m_pToOneArrow);
+
+	m_pToOneMinusArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ToOneMinusArrow"));
+	m_pToOneMinusArrow->SetupAttachment(this);
+	SpanArrowComponent(RelativeLocation, m_OneMinusPosition, m_pToOneMinusArrow);
+#endif
+
+}
+
+void UMovingSceneRoot::TickComponent
+(
+	const float DeltaTime, 
+	ELevelTick TickType,
+	FActorComponentTickFunction *ThisTickFunction
+)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	m_CurrentIntervalPos =  FMath::Fmod(m_CurrentIntervalPos + DeltaTime, m_IntervalLength);
+	const auto LerpFactor{ m_pDynamics->GetFloatValue(m_CurrentIntervalPos) };
+
+	auto NewRelativePos{ m_StartingRootPosition };
+	NewRelativePos +=  m_OnePosition * FMath::Clamp(LerpFactor, 0.f, 1.f);
+	NewRelativePos +=  m_OneMinusPosition * FMath::Clamp(LerpFactor, -1.f, 0.f);
+
+	SetRelativeLocation(NewRelativePos);
+
+
+}
+
+void UMovingSceneRoot::BeginPlay()
+{
+	Super::BeginPlay();
+
+	m_StartingRootPosition = RelativeLocation;
+
+
+}
+
+
+#if WITH_EDITOR
+void UMovingSceneRoot::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	SpanArrowComponent(RelativeLocation, m_OnePosition, m_pToOneArrow);
+	SpanArrowComponent(RelativeLocation, m_OneMinusPosition, m_pToOneMinusArrow);
+
+
+}
+#endif
+	
+
+void SpanArrowComponent(const FVector &From, const FVector &To, UArrowComponent *pArrow)
+{
+	pArrow->SetRelativeLocation(From);
+
+	const auto Disp{ (To - From) };
+	const auto Magnitude{ Disp.Size() };
+	const auto Dir{ Disp.GetSafeNormal() };
+
+	pArrow->SetRelativeRotation(Dir.Rotation());
+	pArrow->SetRelativeScale3D(FVector{ Magnitude/100, 0, 0 });
+
+	
+}
---
+++ Canyon/Source/Canyon/Private/Components/MovingSceneRoot.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "MovingSceneRoot.generated.h"
+
+
+UCLASS(ClassGroup=(Movement, Canyon), Meta=(BlueprintSpawnableComponent), HideCategories=(Velocity))
+class UMovingSceneRoot : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UMovingSceneRoot();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual void BeginPlay() override;
+
+#if WITH_EDITOR	
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+
+protected:
+	UPROPERTY(EditAnywhere, DisplayName="Position On One", Meta=(MakeEditWidget))
+		FVector m_OnePosition;
+		
+	UPROPERTY(EditAnywhere, DisplayName="Position On Minus One", Meta=(MakeEditWidget))
+		FVector m_OneMinusPosition;
+
+	UPROPERTY(EditAnywhere, Instanced, DisplayName="Dynamics")
+		class UCurveFloat *m_pDynamics;
+
+	UPROPERTY(EditAnywhere)
+		float m_IntervalLength;
+
+
+private:
+	float m_CurrentIntervalPos;
+	FVector m_StartingRootPosition;
+
+#if WITH_EDITOR
+	UPROPERTY()
+		class UArrowComponent *m_pToOneArrow;
+
+	UPROPERTY()
+		class UArrowComponent *m_pToOneMinusArrow;
+#endif
+
+
+};
+
+void SpanArrowComponent(const FVector &From, const FVector &To, class UArrowComponent *pArrow);
+
